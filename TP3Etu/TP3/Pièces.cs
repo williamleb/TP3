588ae1aa107c7b1c243e7785e1b86d89407bcf4c
generalkiwi1 <xx-mik-xx@hotmail.com>
2016-11-25 15:06:08 -0500

    Test de push (pièces)

---
+++ TP3Etu/TP3/Pièces.cs
@@ -0,0 +1,28 @@
+﻿//mika gauthier
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3
+{
+  class Pièces
+  {
+    //Énumération contenant le nom des différentes pièces du jeu Tétris ainsi
+    //que le numéro de ces pièces
+    enum PieceTeris
+    {
+      // Les couleurs peuvent être modifiées
+      block, //couleur jaune
+      barreVerticale, //couleur cyan
+      barreHorizontale, //couleur cyan
+      pieceEnT, //couleur mauve
+      pieceEnLDroite, //couleur orange
+      pieceEnLGauche, //couleur bleu foncé
+      pieceEnS, //couleur rouge
+      pieceEnZ, //couleur rose
+    }
+  }
+}
+//mika gauthier
