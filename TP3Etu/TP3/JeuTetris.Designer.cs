8f10217ed7eabd3f351a2d2fe7611adf9bef3db2
generalkiwi1 <xx-mik-xx@hotmail.com>
2016-12-01 09:15:43 -0500

    Modification simple pour le fontionnement de base
    Signed-off-by: generalkiwi1 <xx-mik-xx@hotmail.com>

--- TP3Etu/TP3/JeuTetris.Designer.cs
+++ TP3Etu/TP3/JeuTetris.Designer.cs
@@ -71,7 +71,7 @@
       this.tableauJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
       this.tableauJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
       this.tableauJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-      this.tableauJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 79F));
+      this.tableauJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 89F));
       this.tableauJeu.Dock = System.Windows.Forms.DockStyle.Fill;
       this.tableauJeu.Location = new System.Drawing.Point(240, 0);
       this.tableauJeu.Margin = new System.Windows.Forms.Padding(0);
@@ -186,26 +186,26 @@
       // menuJouer
       // 
       this.menuJouer.Name = "menuJouer";
-      this.menuJouer.Size = new System.Drawing.Size(152, 22);
+      this.menuJouer.Size = new System.Drawing.Size(134, 22);
       this.menuJouer.Text = "Jouer";
       this.menuJouer.Click += new System.EventHandler(this.OnClickMenuJouer);
       // 
       // menuReinitialiser
       // 
       this.menuReinitialiser.Name = "menuReinitialiser";
-      this.menuReinitialiser.Size = new System.Drawing.Size(152, 22);
+      this.menuReinitialiser.Size = new System.Drawing.Size(134, 22);
       this.menuReinitialiser.Text = "Réinitialiser";
       this.menuReinitialiser.Click += new System.EventHandler(this.menuReinitialiser_Click);
       // 
       // toolStripMenuItem1
       // 
       this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-      this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+      this.toolStripMenuItem1.Size = new System.Drawing.Size(131, 6);
       // 
       // menuQuitter
       // 
       this.menuQuitter.Name = "menuQuitter";
-      this.menuQuitter.Size = new System.Drawing.Size(152, 22);
+      this.menuQuitter.Size = new System.Drawing.Size(134, 22);
       this.menuQuitter.Text = "Quitter";
       this.menuQuitter.Click += new System.EventHandler(this.menuQuitter_Click);
       // 
